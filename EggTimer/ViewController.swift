78dfccc1dec935489fe51340fe014b980b1d70c3
Mojtaba <fazeli.mojtaba@gmail.com>
2020-10-19 15:19:34 -0400

    completes the challange

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet var headerTitle: UILabel!
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var count = -1;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+    }
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        count = eggTimes[hardness]!
+    }
+        
+    @objc func update() {
+        if(count > 0) {
+            headerTitle.text = "Come back in\n" + String(count) + "\nseconds"
+            count-=1
+        } else if(count == 0) {
+            headerTitle.text = "Ready!"
+        }
+    }
 }
