a857ba3b0ea305fc1e4f0122b2935817efc4055e
Mojtaba <fazeli.mojtaba@gmail.com>
2020-10-25 21:03:05 -0400

    completes project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,27 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet var headerTitle: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var remainingTime: UILabel!
+    
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var count = -1;
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
-    }
+    var timer = Timer()
+    var totalTime = 0;
+    var hardness = ""
+    var player: AVAudioPlayer!
+    
+   
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
+        if (player != nil) {
+            player.stop()
+        }
+        
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        
+        hardness = sender.currentTitle!
         count = eggTimes[hardness]!
+        totalTime = count
+        
     }
         
     @objc func update() {
         if(count > 0) {
-            headerTitle.text = "Come back in\n" + String(count) + "\nseconds"
+            headerTitle.text = hardness
+            remainingTime.text = String(count)
+            progressBar.progress = Float(count)/Float(totalTime)
             count-=1
         } else if(count == 0) {
+            progressBar.progress = 0.0
             headerTitle.text = "Ready!"
+            remainingTime.text = "0"
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+    }
+    
+    @IBAction func restart(_ sender: Any) {
+        timer.invalidate()
+        progressBar.progress = 1.0
+        headerTitle.text = "How do you like your eggs?"
+        remainingTime.text = "Remaining Time"
+        if (player != nil) {
+            player.stop()
         }
     }
 }
